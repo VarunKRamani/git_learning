b1869a521d62c2324697f7bfc3fb52eea2c2ba94
VarunKRamani <varunramani849@gmail.com>
2024-06-04 14:43:44 +0530

    modify1 app.js

--- app.js
+++ app.js
@@ -1,12 +1,1 @@
-import React, { version } from 'react'
-
-const app = () => {
-    console.log("this is my #rd version")
-  return (
-    <div>
-        <h1>Hello GIT - this is secong version</h1>
-    </div>
-  )
-}
-
-export default app
+console.log("this is my 4th version")
