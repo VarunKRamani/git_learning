b1b36a89a5d148944701f084a8fd11db4f03fea8
VarunKRamani <varunramani849@gmail.com>
2024-06-04 14:39:33 +0530

    modify app.js

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
-import React from 'react'
+import React, { version } from 'react'
 
 const app = () => {
+    console.log("this is my #rd version")
   return (
     <div>
         <h1>Hello GIT - this is secong version</h1>
